7fbb96186cf47019309f8804f425d0cc510ad25e
JacksonSchacher <jschacher8@gmail.com>
2021-10-29 11:04:22 -0600

    first commit

---
+++ Collections.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'plant-collector.us.auth0.com'
+export const clientId = 'A1PdvqVPMkuNKplKufHfGVDKx88GQ2Lw'
+export const audience = 'https://collections.com'
