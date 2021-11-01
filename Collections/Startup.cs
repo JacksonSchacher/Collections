26137477f43eaa380589c72bcbe2d9195bf959d9
JacksonSchacher <jschacher8@gmail.com>
2021-11-01 10:10:26 -0600

    transient

--- Collections/Startup.cs
+++ Collections/Startup.cs
@@ -41,6 +41,9 @@ namespace Collections
 
             services.AddTransient<VaultsRepository>();
             services.AddTransient<VaultsService>();
+
+            services.AddTransient<VaultKeepsRepository>();
+            services.AddTransient<VaultKeepsService>();
         }
 
         private void ConfigureCors(IServiceCollection services)
